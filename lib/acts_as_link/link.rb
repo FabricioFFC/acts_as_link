e5142ad0ddad3d909539ea019a64fa4360022140
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-02-16 16:49:14 -0200

    added Link class in module ActsAsLink

--- lib/acts_as_link/link.rb
+++ lib/acts_as_link/link.rb
@@ -3,71 +3,73 @@ require 'uri'
 require 'rubygems'
 require 'bitly'
 
-class Link
-  
-  IMAGE_EXTENSIONS = %w(.bmp .gif .ico .jpeg .jpg .png .psd .svg .tif .tiff .xcf)
-  
-  def initialize(url)
-    @uri = URI.parse(url)
-    raise 'Link is not an url' if (@uri.class != URI::HTTP && @uri.class != URI::HTTPS)
-  end  
-  
-  def is_broken?
-    response = Net::HTTP.start(@uri.host, @uri.port) { |http| response = http.head(@uri.path.size > 0 ? @uri.path : "/")}  
-    response_code_is_not_valid?(response.code)
-  end
-  
-  def is_an_image?
-    extension = get_extension(@uri.to_s)
-    verify_if_extension_is_an_image(extension)
-  end
-  
-  def shorten
-    shorten_link_with_bitly(@uri.to_s)    
-  end
-  
-private 
-
-  def response_code_is_not_valid?(code) 
-    if code_is_empty(code) || code_is_an_error_code(code)
-      true
-    else
-      false
-    end
-  end  
-  
-  def code_is_empty(code)
-    if code.nil?
-      true
-    else
-      code[0..0] == ""
-    end 
-  end
-  
-  def code_is_an_error_code(code)
-    code[0..0] == "4" || code[0..0] == "5"
-  end
-  
-  def get_extension(uri)
-    point_position = uri.rindex('.')
-    uri_length = uri.length
-    point_position > (uri_length - 6) ? uri[point_position..uri_length] : nil
-  end
-  
-  def verify_if_extension_is_an_image(extension)
-    return false if extension.nil?
-    is_an_image = false
-    IMAGE_EXTENSIONS.each do |ext| 
-      is_an_image = (extension == ext)
-      break if is_an_image
-    end
-    is_an_image
-  end
-  
-  def shorten_link_with_bitly(link)
-    Bitly.use_api_version_3
-    bitly = Bitly.new('actsaslink', 'R_fc70af6573e064c0f4081317bb592f18')
-    bitly.shorten(link).short_url
-  end
+module ActsAsLink
+  class Link
     
+    IMAGE_EXTENSIONS = %w(.bmp .gif .ico .jpeg .jpg .png .psd .svg .tif .tiff .xcf)
+    
+    def initialize(url)
+      @uri = URI.parse(url)
+      raise 'Link is not an url' if (@uri.class != URI::HTTP && @uri.class != URI::HTTPS)
+    end  
+    
+    def is_broken?
+      response = Net::HTTP.start(@uri.host, @uri.port) { |http| response = http.head(@uri.path.size > 0 ? @uri.path : "/")}  
+      response_code_is_not_valid?(response.code)
+    end
+    
+    def is_an_image?
+      extension = get_extension(@uri.to_s)
+      verify_if_extension_is_an_image(extension)
+    end
+    
+    def shorten
+      shorten_link_with_bitly(@uri.to_s)    
+    end
+    
+  private 
+
+    def response_code_is_not_valid?(code) 
+      if code_is_empty(code) || code_is_an_error_code(code)
+        true
+      else
+        false
+      end
+    end  
+    
+    def code_is_empty(code)
+      if code.nil?
+        true
+      else
+        code[0..0] == ""
+      end 
+    end
+    
+    def code_is_an_error_code(code)
+      code[0..0] == "4" || code[0..0] == "5"
+    end
+    
+    def get_extension(uri)
+      point_position = uri.rindex('.')
+      uri_length = uri.length
+      point_position > (uri_length - 6) ? uri[point_position..uri_length] : nil
+    end
+    
+    def verify_if_extension_is_an_image(extension)
+      return false if extension.nil?
+      is_an_image = false
+      IMAGE_EXTENSIONS.each do |ext| 
+        is_an_image = (extension == ext)
+        break if is_an_image
+      end
+      is_an_image
+    end
+    
+    def shorten_link_with_bitly(link)
+      Bitly.use_api_version_3
+      bitly = Bitly.new('actsaslink', 'R_fc70af6573e064c0f4081317bb592f18')
+      bitly.shorten(link).short_url
+    end
+      
+  end
 end
--- spec/acts_as_link/link_spec.rb
+++ spec/acts_as_link/link_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
-describe Link do
+describe ActsAsLink::Link do
   
   describe 'initialize' do
     
     it 'should initialize with uri variable defined' do
-      link = Link.new('http://google.com')
+      link = ActsAsLink::Link.new('http://google.com')
       link.instance_variable_get('@uri').should_not be_nil
     end
     
     it 'should initialize with uri variable defined when link has https' do
-      link = Link.new('https://rubygems.org/gems/acts_as_link')
+      link = ActsAsLink::Link.new('https://rubygems.org/gems/acts_as_link')
       link.instance_variable_get('@uri').should_not be_nil
     end
     
     it 'should return an exception when try to initialize with an empty string' do
-      lambda{Link.new('')}.should raise_error(RuntimeError,/Link is not an url/)
+      lambda{ActsAsLink::Link.new('')}.should raise_error(RuntimeError,/Link is not an url/)
     end
     
     it 'should return an exception when try to initialize with a string is not a link' do
-      lambda{Link.new('test.com.br')}.should raise_error(RuntimeError,/Link is not an url/)
+      lambda{ActsAsLink::Link.new('test.com.br')}.should raise_error(RuntimeError,/Link is not an url/)
     end
     
   end
@@ -27,12 +27,12 @@ describe Link do
   describe 'is_broken?' do
     
     it 'should return false when link is live' do
-      link = Link.new('http://google.com')
+      link = ActsAsLink::Link.new('http://google.com')
       link.is_broken?.should be_false
     end
     
     it 'should return true when link is broken' do
-      link = Link.new('http://www.vizir.com.br/not_exists_page')
+      link = ActsAsLink::Link.new('http://www.vizir.com.br/not_exists_page')
       link.is_broken?.should be_true
     end
     
@@ -41,28 +41,28 @@ describe Link do
   describe 'is_an_image?' do
     
     it 'should return true when url links to an image' do
-      link = Link.new('http://www.google.com/images/srpr/nav_logo35.png')
+      link = ActsAsLink::Link.new('http://www.google.com/images/srpr/nav_logo35.png')
       link.is_an_image?.should be_true
     end
     
     it 'should return false when url does not link to an image' do
-      link = Link.new('http://www.google.com')
+      link = ActsAsLink::Link.new('http://www.google.com')
       link.is_an_image?.should be_false
     end
     
     it 'should return false when url links to a pdf file' do
-      link = Link.new('http://qualidadebr.files.wordpress.com/2009/06/livro-qualidadebr.pdf')
+      link = ActsAsLink::Link.new('http://qualidadebr.files.wordpress.com/2009/06/livro-qualidadebr.pdf')
       link.is_an_image?.should be_false
     end
     
     it 'should return false when url extension image is not in the end' do
-      link = Link.new('http://www.google.com/images/srpr/nav_.pnglogo35')
+      link = ActsAsLink::Link.new('http://www.google.com/images/srpr/nav_.pnglogo35')
       link.is_an_image?.should be_false
     end
     
     it 'should return true for all image extensions defined' do
-      Link::IMAGE_EXTENSIONS.each do |ext|
-        link = Link.new("http://www.google.com/images/srpr/nav_logo35#{ext}")
+      ActsAsLink::Link::IMAGE_EXTENSIONS.each do |ext|
+        link = ActsAsLink::Link.new("http://www.google.com/images/srpr/nav_logo35#{ext}")
         link.is_an_image?.should be_true
       end
     end
@@ -72,7 +72,7 @@ describe Link do
   describe 'shorten' do
     
     it 'should return shortened link' do
-      link = Link.new('http://www.google.com')
+      link = ActsAsLink::Link.new('http://www.google.com')
       link.shorten.include?('bit.ly').should be_true
     end
     
